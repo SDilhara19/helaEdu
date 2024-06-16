ba9c0953c9020b520ad56fdee620cc23c55c51fa
NirmalHettiarachchi <nirmalhettiarachchi5@gmail.com>
2024-06-16 13:17:20 +0530

    Add Firebase config and restructure application

--- server/src/main/java/com/helaedu/website/WebsiteApplication.java
+++ server/src/main/java/com/helaedu/website/WebsiteApplication.java
@@ -1,34 +1,12 @@
 package com.helaedu.website;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Objects;
 
 @SpringBootApplication
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
-		ClassLoader classLoader = WebsiteApplication.class.getClassLoader();
-
-		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
-
-		try (FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath())) {
-			FirebaseOptions options = new FirebaseOptions.Builder()
-					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-					.build();
-
-			FirebaseApp.initializeApp(options);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
 		SpringApplication.run(WebsiteApplication.class, args);
 	}
 
---
+++ server/src/main/java/com/helaedu/website/entity/Test.java
@@ -0,0 +1,12 @@
+package com.helaedu.website.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Test {
+    private String document_id;
+    private String name;
+    private int age;
+}
