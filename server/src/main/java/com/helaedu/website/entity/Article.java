028c4b01ffbb26751ed2657665a3e624295d76da
M.K.P.Ahinsa <pathuahinsa2001@gmail.com>
2024-06-18 21:14:33 +0530

    add Changes in article entity

--- server/src/main/java/com/helaedu/website/entity/Article.java
+++ server/src/main/java/com/helaedu/website/entity/Article.java
@@ -1,2 +1,24 @@
-package com.helaedu.website.entity;public class Article {
+package com.helaedu.website.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Article {
+    private String articleId;
+    private String title;
+    private ArrayList<String> tags;
+    private String content;
+    private String img;
+    private String additionalFile;
+    private String teacherId;
+    private String articleStatus;
 }
