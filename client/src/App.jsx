d1379d0c66a8d9e74f8d4feb8f0da0fd18c35e08
SDilhara19 <sdilharabandara@gmail.com>
2024-06-26 10:10:49 +0530

    quiz

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Quiz from "./pages/quizes/Quiz";
+
+
+
 function App() {
   return (
     <div>
-      <Header />
+      <BrowserRouter>
+      <Routes>
+        <Route path='/quiz' element={<Quiz/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue" : "#0A6CF5",
+        'yellow' : '#F5A711',
+        'white' : '#FFFFFF',
+        'black' : '#000000'
+      },
+    },
   },
-  plugins: [],
+  darkMode: "class", // Enable dark mode support
+  plugins: [
+    require("nightwind") // Add the nightwind plugin
+  ],
 };
