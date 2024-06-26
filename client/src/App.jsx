68453303f188b9127ba86848a09d04f10826bc99
LNTR <101091694+LNTR@users.noreply.github.com>
2024-06-26 14:31:29 +0530

    modified:   client/src/App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,18 @@
-
 import { Landing } from "@/components/landing";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Quiz from "./pages/quizes/Quiz";
 
 function App() {
   return (
     <div>
-    <BrowserRouter>
-      <Routes>
-    {/* http://localhost:5173/landing */}
-    <Route path="/" element={<Landing />}></Route>
-    {/* http://localhost:5173/quiz */}
-        <Route path='/quiz' element={<Quiz/>}></Route>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />}></Route>
+          <Route path="/quiz" element={<Quiz />}></Route>
+        </Routes>
       </BrowserRouter>
-      
+    </div>
+  );
+}
 
 export default App;
-
-
