b66309db00687b032ee5def65905b5ab118b2b4d
LNTR <101091694+LNTR@users.noreply.github.com>
2024-06-26 10:44:30 +0530

    modified:   client/src/components/common/Footer.jsx 	modified:   client/src/styles/_catalog.scss 	modified:   client/src/styles/_common.scss 	modified:   client/src/styles/_footer.scss

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,41 +1,71 @@
 import React from "react";
 import logo from "@assets/icons/logo_white.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faLinkedin,
+  faTwitter,
+  faInstagram,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <footer>
       <div>
-        <nav className="flex-sb">
+        <nav className="flex-sb-start">
           <div className="flex-c">
             <img src={logo} alt="" srcset="" />
           </div>
-          <div className="flex-col-start">
+          <div className="flex-col-start nav-link-wrapper">
             <h2>HelaEdu</h2>
             <ul>
-              <li>About Us</li>
-              <li>About Us</li>
-              <li>About Us</li>
+              <li>
+                <h4>About Us</h4>
+              </li>
+              <li>
+                <h4>Terms of service</h4>
+              </li>
+              <li>
+                <h4>Privacy policy</h4>
+              </li>
             </ul>
           </div>
-          <div className="flex-col-start">
-            <h2>HelaEdu</h2>
+          <div className="flex-col-start nav-link-wrapper">
+            <h2>Get Help</h2>
             <ul>
-              <li>About Us</li>
-              <li>About Us</li>
-              <li>About Us</li>
+              <li>
+                <h4>FAQ</h4>
+              </li>
+              <li>
+                <h4>Payment options</h4>
+              </li>
             </ul>
           </div>
-          <div className="flex-col-start">
-            <h2>HelaEdu</h2>
-            <ul>
-              <li>About Us</li>
-              <li>About Us</li>
-              <li>About Us</li>
+          <div className="flex-col-start nav-link-wrapper">
+            <h2>Get Help</h2>
+
+            <ul className="flex-sb w-full">
+              <li>
+                <FontAwesomeIcon icon={faFacebook} />
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faLinkedin} />
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faTwitter} />
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faYoutube} />
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faInstagram} />
+              </li>
             </ul>
           </div>
         </nav>
       </div>
-      <div className="flex-c w-full">
+      <div className="flex-c copyright-text">
         <h4>HelaEdu Team</h4>
       </div>
     </footer>
