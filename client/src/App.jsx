936cceabb9ae83716fcb96bce735c8d543fbde2b
LNTR <101091694+LNTR@users.noreply.github.com>
2024-06-30 17:50:58 +0530

    modified:   client/src/App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import { Landing } from "@/components/landing";
-import {  Route, Routes } from 'react-router-dom';
-import Quiz from "./pages/quizes/Quiz";
-import Articles from "./pages/articles/Articles";
-import ReadArticles from "./pages/articles/ReadArticles";
+import { Route, Routes } from "react-router-dom";
+import Quiz from "@pages/quizes/Quiz";
+import Articles from "@pages/articles/Articles";
+import ReadArticles from "@pages/articles/ReadArticles";
 import Auth from "@pages/auth/Auth";
-import AddArticles from "./pages/articles/AddArticles";
-import AddArticleForm from "./pages/articles/AddArticleForm";
-import ReviewList from "./pages/articles/reviewList";
-import ReviewArticle from "./pages/articles/ReviewArticle";
-import AssignmentList from "./pages/assignments/AssignmentList";
-import createAssignments from "@pages/assignments/createAssignments";
+import AddArticles from "@pages/articles/AddArticles";
+import AddArticleForm from "@pages/articles/AddArticleForm";
+import ReviewList from "@pages/articles/reviewList";
+import ReviewArticle from "@pages/articles/ReviewArticle";
+import AssignmentList from "@pages/assignments/AssignmentList";
+import CreateAssignments from "@pages/assignments/createAssignments";
 function App() {
   return (
     <div>
@@ -23,8 +23,11 @@ function App() {
         <Route path="/addArticleForm" element={<AddArticleForm />}></Route>
         <Route path="/reviewList" element={<ReviewList />}></Route>
         <Route path="/reviewArticle" element={<ReviewArticle />}></Route>
-        <Route path="/assignmentList" element={<AssignmentList/>}></Route>
-        <Route path="/createAssignments" element={<createAssignments/>}></Route>
+        <Route path="/assignmentList" element={<AssignmentList />}></Route>
+        <Route
+          path="/createAssignments"
+          element={<CreateAssignments />}
+        ></Route>
       </Routes>
     </div>
   );
