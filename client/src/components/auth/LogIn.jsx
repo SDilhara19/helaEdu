050a3a2790bcb13a88bfb40d14bf2f48474e150e
LNTR <101091694+LNTR@users.noreply.github.com>
2024-07-02 16:17:05 +0530

    Auth jwt complete

--- client/src/components/auth/LogIn.jsx
+++ client/src/components/auth/LogIn.jsx
@@ -3,23 +3,63 @@ import useSignIn from "react-auth-kit/hooks/useSignIn";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import logo from "@assets/icons/logo.svg";
 import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
+import { authenticateUser } from "@services/AuthService";
 
 function Login() {
+  const [formData, setFormData] = React.useState({ email: "", password: "" });
+  const signIn = useSignIn();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    authenticateUser(formData).then((res) => {
+      if (res.status === 200) {
+        if (
+          signIn({
+            auth: {
+              token: res.data.jwt,
+              type: "Bearer",
+            },
+            refresh: res.data.jwt,
+          })
+        ) {
+        } else {
+        }
+      }
+    });
+  };
   return (
     <>
-      <div className="login-left-pannel flex-c">
+      <form
+        method="POST"
+        className="login-left-pannel flex-c"
+        onSubmit={onSubmit}
+      >
         <div className="login-details flex-col-c">
           <h2 className="m-3">Hello</h2>
           <h3 className="m-3">Sign into your account</h3>
           <div className="m-3 w-full">
-            <label class="flex text-input m-3">
+            <label className="flex text-input m-3">
               <FontAwesomeIcon icon={faEnvelope} size="1.5x" className="icon" />
-              <input type="text" name="email" placeholder="Email" />
+              <input
+                type={"email"}
+                name="email"
+                placeholder="Email"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+              />
             </label>
             <h5>&nbsp;</h5>
-            <label class="flex text-input m-3">
+            <label className="flex text-input m-3">
               <FontAwesomeIcon icon={faLock} size="1.5x" className="icon" />
-              <input type="text" name="password" placeholder="Password" />
+              <input
+                type={"password"}
+                name="password"
+                placeholder="Password"
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+              />
             </label>
             <h5>&nbsp;</h5>
           </div>
@@ -41,7 +81,7 @@ function Login() {
             </h4>
           </div>
         </div>
-      </div>
+      </form>
 
       <div className="login-right-pannel flex-c">
         <div className="login-banner flex-col-c">
