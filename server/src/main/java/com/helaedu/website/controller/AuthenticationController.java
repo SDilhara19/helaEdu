0d09ad97a65b6256eb193e95e504451d5f5ce4eb
LNTR <101091694+LNTR@users.noreply.github.com>
2024-07-03 15:39:28 +0530

    modified:   server/src/main/java/com/helaedu/website/controller/AuthenticationController.java

--- server/src/main/java/com/helaedu/website/controller/AuthenticationController.java
+++ server/src/main/java/com/helaedu/website/controller/AuthenticationController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/authenticate")
+@CrossOrigin(origins = "*")
 public class AuthenticationController {
 
     @Autowired
