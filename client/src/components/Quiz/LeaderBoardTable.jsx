a4d20936a6da47e140ee36f4813d57c39876b599
SDilhara19 <sdilharabandara@gmail.com>
2024-07-04 05:29:50 +0530

    leaderboard

--- client/src/components/Quiz/LeaderBoardTable.jsx
+++ client/src/components/Quiz/LeaderBoardTable.jsx
@@ -4,27 +4,60 @@ const LeaderBoardTable = () => {
   return (
     <div>
       <div className='w-4/5 mx-auto my-10'>
-        <div class="table-main">
+        <div class="table-main text-1">
           <div class="thead-main">
-            <div class="th-main">Header 1</div>
-            <div class="th-main">Header 2</div>
-            <div class="th-main">Header 3</div>
+   
           </div>
           <div class="tbody-main">
             <div class="tr-main">
-              <div class="td-main">Row 1, Col 1</div>
-              <div class="td-main">Row 1, Col 2 Row 1, Col 2Row 1, Col 2Row 1, Col 2Row 1, Col 2Row 1, Col 2Row 1, Col 2</div>
-              <div class="td-main">Row 1, Col 3</div>
+              <div class="td-main h-full flex items-center">
+                <div className="rank-circle">1</div>
+
+              </div>
+              <div class="td-main">
+                <div className='flex items-center p-0 border'>
+                  <div className="avatar m-0">
+                    <div className="w-12 rounded-full">
+                      <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+                  </div>
+                  <div className='mx-4'>Sanduni</div>
+                </div>
+              </div>
+              <div class="td-main">1530</div>
             </div>
             <div class="tr-main">
-              <div class="td-main">Row 2, Col 1</div>
-              <div class="td-main">Row 2, Col 2</div>
-              <div class="td-main">Row 2, Col 3</div>
+              <div class="td-main h-full flex items-center">
+                <div className="rank-circle">2</div>
+              </div>
+              <div class="td-main">
+                <div className='flex items-center p-0 border'>
+                  <div className="avatar m-0">
+                    <div className="w-12 rounded-full">
+                      <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+                  </div>
+                  <div className='mx-4'>Pathumi</div>
+                </div>
+              </div>
+              <div class="td-main">1390</div>
             </div>
             <div class="tr-main">
-              <div class="td-main">Row 3, Col 1</div>
-              <div class="td-main">Row 3, Col 2</div>
-              <div class="td-main">Row 3, Col 3</div>
+              <div class="td-main h-full flex items-center">
+                <div className="rank-circle">3</div>
+
+              </div>
+              <div class="td-main">
+                <div className='flex items-center p-0 border'>
+                  <div className="avatar m-0">
+                    <div className="w-12 rounded-full">
+                      <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+                  </div>
+                  <div className='mx-4'>Nirmal</div>
+                </div>
+              </div>
+              <div class="td-main">1290</div>
             </div>
           </div>
         </div>
