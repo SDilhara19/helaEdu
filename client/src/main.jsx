08bca27ba749b631d988900c3b0545543059c101
LNTR <101091694+LNTR@users.noreply.github.com>
2024-07-11 10:45:17 +0530

    Fixed issues with respository

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,14 +6,9 @@ import "@styles/light_theme.scss";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "react-auth-kit";
 import authStore from "@/utils/auth.js";
-<<<<<<< HEAD
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import allReducers from "@reducers/index.js";
-=======
-// import { configureStore } from "@reduxjs/toolkit";
-// import { Provider } from "react-redux";
->>>>>>> c1b75534c8dd9bfbbe76fa33f127eb74fde7de47
 
 const store = configureStore({
   reducer: allReducers,
