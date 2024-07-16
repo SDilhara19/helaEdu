4b4a8e5886829c289fdebf75f6b007e4d5664fb2
LNTR <101091694+LNTR@users.noreply.github.com>
2024-07-16 22:33:26 +0530

    Intergrated firebase with flask

--- service/app.py
+++ service/app.py
@@ -1,8 +1,25 @@
 from flask import Flask
+from routes.chat import chat
+from routes.status_codes import status_codes
+from flask_cors import CORS
 
-app = Flask(__name__)
+from firebase_admin import credentials
+import firebase_admin
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def create_app():
+    cred = credentials.Certificate("config/credentials.json")
+    firebase_admin.initialize_app(cred)
+
+    app = Flask(__name__)
+    CORS(app, resources={"/*": {"origins": "*"}})
+
+    app.register_blueprint(chat, url_prefix="/chat")
+    app.register_blueprint(status_codes, url_prefix="/status_code")
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True, port=8081)
