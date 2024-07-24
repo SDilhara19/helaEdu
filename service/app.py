1ed55c1fef0ae7d47cb9570da913849409dd77a9
LNTR <101091694+LNTR@users.noreply.github.com>
2024-07-24 17:43:13 +0530

    Firebase db connected

--- service/app.py
+++ service/app.py
@@ -3,13 +3,14 @@ from Blueprint.chat import chat
 from Blueprint.status_codes import status_codes
 from flask_cors import CORS
 
+
 import fireo
 
 
 def create_app():
     app = Flask(__name__)
     CORS(app, resources={"/*": {"origins": "*"}})
-    fireo.connection(from_file="config/credentials.json")
+    conn = fireo.connection(from_file="config/credentials.json")
 
     app.register_blueprint(chat, url_prefix="/chat")
     app.register_blueprint(status_codes, url_prefix="/status_code")
