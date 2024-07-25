e28e9ff602bf14530749b6061049b9ce0e358978
NirmalHettiarachchi <nirmalhettiarachchi5@gmail.com>
2024-07-25 06:13:47 +0530

    Restrict access to teacher proof files to admins only

--- server/src/main/java/com/helaedu/website/service/FirebaseStorageService.java
+++ server/src/main/java/com/helaedu/website/service/FirebaseStorageService.java
@@ -1,13 +1,13 @@
 package com.helaedu.website.service;
 
-import com.google.cloud.storage.Acl;
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageException;
+import com.google.cloud.storage.*;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 @Service
@@ -32,6 +32,18 @@ public class FirebaseStorageService {
         }
     }
 
+    public String uploadTeacherProof(MultipartFile file, String email) throws IOException {
+        String blobName = "teacher_proofs/" + email + "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+        BlobInfo blobInfo = BlobInfo.newBuilder("helaedu-website.appspot.com", blobName).build();
+        try {
+            storage.create(blobInfo, file.getBytes());
+            return blobName;
+        } catch (StorageException e) {
+            System.err.println("Error uploading file to Google Cloud Storage: " + e.getMessage());
+            throw e;
+        }
+    }
+
     public String uploadProfilePicture(MultipartFile file, String email) throws IOException {
         return uploadFile(file, "profile_pictures/" + email);
     }
@@ -44,7 +56,17 @@ public class FirebaseStorageService {
         return uploadFile(file, "article_additional_files/" + articleId);
     }
 
-    public String uploadTeacherProof(MultipartFile file, String email) throws IOException {
-        return uploadFile(file, "teacher_proofs/" + email);
+//    public String uploadTeacherProof(MultipartFile file, String email) throws IOException {
+//        return uploadFile(file, "teacher_proofs/" + email);
+//    }
+
+    public Resource getFileAsResource(String fileName) throws IOException {
+        Blob blob = storage.get("helaedu-website.appspot.com", fileName);
+        if (blob == null) {
+            throw new FileNotFoundException("File not found: " + fileName);
+        }
+
+        byte[] content = blob.getContent();
+        return new ByteArrayResource(content);
     }
 }
