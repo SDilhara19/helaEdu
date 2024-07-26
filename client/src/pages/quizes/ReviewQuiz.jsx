6406615fe470b176967c186faad5256bdd077a97
LNTR <101091694+LNTR@users.noreply.github.com>
2024-07-26 18:25:01 +0530

    modified:   src/pages/quizes/ReviewQuiz.jsx

--- client/src/pages/quizes/ReviewQuiz.jsx
+++ client/src/pages/quizes/ReviewQuiz.jsx
@@ -1,20 +1,17 @@
-
-import React from 'react'
+import React from "react";
 import { Header, Footer } from "@/components/common";
-import Question from '@/components/Quiz/Question'
-import Answers from '@/components/Quiz/Answers';
-import RegenarateButton from '@/components/Quiz/RegenarateButton';
-
-
+import Question from "@/components/Quiz/Question";
+import Answers from "@/components/Quiz/Answer";
+import RegenarateButton from "@/components/Quiz/RegenarateButton";
 
 const Quiz = () => {
   return (
     <>
-    <Header/>
-    <Question/>
-    <Answers/>
-    <RegenarateButton/>
-    <Footer/>
+      <Header />
+      <Question />
+      <Answers />
+      <RegenarateButton />
+      <Footer />
     </>
   );
 };
