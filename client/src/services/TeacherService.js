d85cb7108f450635e3f2f3568048824b3fed1027
LNTR <101091694+LNTR@users.noreply.github.com>
2024-07-26 17:06:14 +0530

    Common header changed

--- client/src/services/TeacherService.js
+++ client/src/services/TeacherService.js
@@ -1,5 +1,14 @@
 import axios from "axios";
+import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
+const authHeader = useAuthHeader();
+const headers = {
+  Authorization: authHeader,
+};
 const GET_USER_DETAILS_URL = `${import.meta.env.VITE_REST_API_BASE_URL}/tm`;
-const REST_API_BASE_URL = `${import.meta.env.VITE_REST_API_BASE_URL}/teachers/me`;
-export const listTeacherDetails = (headers) => axios.get(REST_API_BASE_URL, { headers });
-export const getUserDetails = (userId) => axios.get(`${GET_USER_DETAILS_URL}/${userId}`);
+const REST_API_BASE_URL = `${
+  import.meta.env.VITE_REST_API_BASE_URL
+}/teachers/me`;
+export const listTeacherDetails = () =>
+  axios.get(REST_API_BASE_URL, { headers });
+export const getUserDetails = (userId) =>
+  axios.get(`${GET_USER_DETAILS_URL}/${userId}`);
