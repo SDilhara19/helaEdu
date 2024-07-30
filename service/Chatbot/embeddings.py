451397eac2914da9dd56438f1d8761fa3e0f1185
SDilhara19 <sdilharabandara@gmail.com>
2024-07-30 08:29:38 +0530

    embedded textbooks to the vectorestore

--- service/Chatbot/embeddings.py
+++ service/Chatbot/embeddings.py
@@ -1,5 +1,66 @@
-from lang_funcs import *
+# from lang_funcs import *
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.vectorstores import FAISS
 from PyPDF2 import PdfReader, PdfWriter
+import os
+
+def load_embedding_model(model_path, normalize_embedding=True):
+    return HuggingFaceEmbeddings(
+        model_name=model_path,
+        model_kwargs={"device": "cpu"},  # here we will run the model with CPU only
+        encode_kwargs={
+            "normalize_embeddings": normalize_embedding  # keep True to compute cosine similarity
+        },
+    )
+
+def load_pdf_data(file_path):
+    # Creating a PyMuPDFLoader object with file_path
+    loader = PyMuPDFLoader(file_path=file_path)
+
+    # loading the PDF file
+    docs = loader.load()
+
+    # returning the loaded document
+    return docs
+
+def split_docs(documents, chunk_size=1000, chunk_overlap=200):
+
+    # Initializing the RecursiveCharacterTextSplitter with
+    # chunk_size and chunk_overlap
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+    )
+
+    # Splitting the documents into chunks
+    chunks = text_splitter.split_documents(documents=documents)
+
+    # returning the document chunks
+    return chunks
+
+def create_embeddings(chunks, embedding_model, storing_path="service/Chatbot/vectorstore/10"):
+    # Creating the embeddings using FAISS
+    if os.path.exists(storing_path):
+        embeddings = load_embedding_model(model_path="all-MiniLM-L6-v2")
+        # Load the existing vectorstore
+        vectorstore = FAISS.load_local(storing_path, embeddings, allow_dangerous_deserialization=True)
+        # Add new embeddings to the existing vectorstore
+        # vectorstore.add_documents(chunks)
+        if   vectorstore.add_documents(chunks):
+            # Save the updated vectorstore
+            
+            print("Updated vectorstore")
+
+    else:
+        # Create a new vectorstore
+        vectorstore = FAISS.from_documents(chunks, embedding_model)
+
+    # Save the updated vectorstore
+    vectorstore.save_local(storing_path)
+
+    return True
+
 
 # Function for creating embeddings using FAISS
 def main():
@@ -7,7 +68,7 @@ def main():
     embed = load_embedding_model(model_path="all-MiniLM-L6-v2")
 
     docs = load_pdf_data(
-        file_path="/Users/helaEdu/textbooks/10/ICT.pdf"
+        file_path="/Users/helaEdu/textbooks/10/Maths_II.pdf"
     )
 
     documents = split_docs(documents=docs)
